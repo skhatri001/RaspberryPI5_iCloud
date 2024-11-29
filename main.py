1f167d461bbe54b12134be5fc0d77370d68e6b58
skhatri007 <skhatri007@outlook.com>
2024-11-29 07:36:35 -0500

    Included shlex needed to passing multiple arguments for running a shell script.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import subprocess
 import numpy as np
 import os
+import shlex
 os.chdir('/home/skhatri007/Dropbox-Uploader')
 # os.getcwd()
-output = subprocess.call(['./dropbox_uploader.sh','list /'])
+#subprocess.call(shlex.split('./test.sh param1 param2'))
+
+output = subprocess.run(shlex.split('./dropbox_uploader.sh list /'))
 print(output)
