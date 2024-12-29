e93fda18332455939fbd92ce86f357bf25e5d560
skhatri001 <skhatri007@outlook.com>
2024-12-29 12:43:28 -0500

    Failed attempt to show image on the local server.

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import dropbox
+import dropbox.files
+import os
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
 def hello_world():
     return "<p>Hello world</p>"
+
+
+@app.route("/")
+@app.route("/index")
+def show_index():
+	full_filename = os.getcwd() + '/' +'IMG_0465.jpg'
+	return render_template("index.html", user_image = full_filename)
+
+
